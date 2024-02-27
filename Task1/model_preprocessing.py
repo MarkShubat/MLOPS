7384eb1b9f0ae4c152471d35bef10ae18de5bd3d
Roflanlico <92446877+Roflanlico@users.noreply.github.com>
2024-02-27 23:30:56 +0500

    Add files via upload

---
+++ Task1/model_preprocessing.py
@@ -0,0 +1,29 @@
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import numpy as np
+import os
+import pandas as pd
+  
+df = pd.read_csv("titanic.csv")
+X = df.drop("Survived", axis=1)
+
+numerical_cols = [cname for cname in X.columns if X[cname].dtype in ['int64', 'float64']]
+categorical_cols = [cname for cname in X.columns if X[cname].dtype == "object"]
+
+numerical_transformer = SimpleImputer(strategy='median')
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+
+
+
