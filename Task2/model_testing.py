a89adbe6d51c98ce2539c5b742d14c746548bda7
Roflanlico <92446877+Roflanlico@users.noreply.github.com>
2024-03-15 22:45:09 +0500

    Update model_testing.py

--- Task2/model_testing.py
+++ Task2/model_testing.py
@@ -11,4 +11,7 @@ import pandas as pd
 
 pipeline = load('pipeline.pkl')
 X_test = pd.read_csv("test/X_test.csv")
+y_test = pd.read_csv("test/y_test.csv")
 predictions = pipeline.predict(X_test)
+
+print('Model accuracy score:', accuracy_score(y_test, predictions))
