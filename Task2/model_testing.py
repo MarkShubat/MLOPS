66d165dde1a7d25aee694e337aa6628f184ece61
Roflanlico <92446877+Roflanlico@users.noreply.github.com>
2024-03-15 22:40:33 +0500

    Update model_testing.py

--- Task2/model_testing.py
+++ Task2/model_testing.py
@@ -3,11 +3,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from model_preprocessing import preprocessor
 from sklearn.metrics import accuracy_score
-from model_preparation import pipeline
-
+#from model_preparation import pipeline
+from joblib import load
 import numpy as np
 import os
 import pandas as pd
 
+pipeline = load('pipeline.pkl')
 X_test = pd.read_csv("test/X_test.csv")
 predictions = pipeline.predict(X_test)
